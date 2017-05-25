b25b367d193de7456c1f33cd1019300a06d0cb0a
Tommy McCormick <mccormickt9@gmail.com>
2017-05-25 12:20:38 -0400

    Fixed project structure

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -56,9 +56,4 @@ public class Person2 {
 	public String toString(String input) {
 	  return name + calc(input);
 	}
-
-	public static void main(String[] args) {
-	    Person2 person = new Person2("Tommy");
-	    person.calc(person.name);
-    }
 }
