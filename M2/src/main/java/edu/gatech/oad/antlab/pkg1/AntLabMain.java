57c96b5ff26fc72209940772311a76053b04b542
ChangedNameTo <william.m95@gmail.com>
2017-05-30 13:38:48 -0400

    master #comment Minor change

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -60,7 +60,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("Tommy McCormick");
-		  toPrint += p2.toString("tmccormick9");
+		  toPrint += p2.toString("tmccormick30");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Amir Ghorbanian");
