7f85c55d90d0389cb0f560342e3681a854d9314a
Andrey Lukin <andrey@Andreys-MacBook-Pro.local>
2017-06-22 00:36:09 -0400

    Initial recycler view ready

---
+++ app/src/main/java/com/example/maddy/maddylogin/Item.java
@@ -0,0 +1,21 @@
+package com.example.maddy.maddylogin;
+
+/**
+ * Created by andrey on 6/21/17.
+ */
+
+public class Item {
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
