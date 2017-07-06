e53266724e9a23d8bbfcd490d55e0bcb7ff0b31d
ChangedNameTo <william.m95@gmail.com>
2017-07-06 15:49:41 -0400

    map_init #comment Spun up the mapping hooks, everyone should be able to start tickets now

---
+++ app/src/main/java/com/example/RB/rebasejumpers/MapsActivity.java
@@ -0,0 +1,50 @@
+package com.example.RB.rebasejumpers;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * The type Maps activity.
+ */
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        GoogleMap mMap = googleMap;
+
+        // Sets default zooms
+        mMap.setMinZoomPreference(12.0f);
+        mMap.setMaxZoomPreference(14.0f);
+
+        LatLng atlanta = new LatLng(33.779721, -84.399186);
+        mMap.addMarker(new MarkerOptions().position(atlanta).title("Marker in Atlanta"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(atlanta));
+    }
+}
